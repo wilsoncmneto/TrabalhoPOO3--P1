4c82c4a9f4caee0d7dfcebe7430e33c6dc535d2d
wilsoncmneto <wilsonneto454@gmail.com>
2025-10-06 00:42:44 -0300

    Add files via upload

---
+++ ProvaPOO3/src/main/java/org/provapoo3/model/fornecedor.java
@@ -0,0 +1,12 @@
+package org.provapoo3.model;
+
+public class fornecedor {
+    private String cnpj;
+    private String razaoSocial;
+    private String telefone;
+    private String email;
+    private String cidade;
+    private String estado;
+
+
+}
