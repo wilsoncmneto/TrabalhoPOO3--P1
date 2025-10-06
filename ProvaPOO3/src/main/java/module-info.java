53262858823f3e1fd534733eefde393f9232641b
wilsoncmneto <wilsonneto454@gmail.com>
2025-10-06 11:24:44 -0300

    Update module-info.java

--- ProvaPOO3/src/main/java/module-info.java
+++ ProvaPOO3/src/main/java/module-info.java
@@ -2,9 +2,11 @@ module org.provapoo3 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens org.provapoo3 to javafx.fxml;
     exports org.provapoo3;
     exports org.provapoo3.controller;
+    exports org.provapoo3.model;
+    
+    opens org.provapoo3 to javafx.fxml;
     opens org.provapoo3.controller to javafx.fxml;
+    opens org.provapoo3.model to javafx.base;
 }
